946b0a596560b7ed545242cb5bd28cc61612220b
Matt York <my0373@gmail.com>
2015-03-26 22:58:07 +0000

    Will be a logfile generator for the imsi stripper.
    Will be a logfile generator for the imsi stripper. 
    
    Only when I get around to it.

---
+++ diameter_logfile_generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+## List of diameter errors when I can be bothered to make a log file generator
+DIAMETER_ERRORS = ["DIAMETER_AVP_UNSUPPORTED", 
+                  "DIAMETER_UNKNOWN_SESSION_ID", 
+                  "DIAMETER_AUTHORIZATION_REJECTED",
+                  "DIAMETER_INVALID_AVP_VALUE", 
+                  "DIAMETER_MISSING_AVP", 
+                  "DIAMETER_RESOURCES_EXCEEDED", 
+                  "DIAMETER_CONTRADICTING_AVPS", 
+                  "DIAMETER_AVP_NOT_ALLOWED", 
+                  "DIAMETER_AVP_OCCURS_TOO_MANY_TIMES",
+                  "DIAMETER_NO_COMMON_APPLICATION", 
+                  "DIAMETER_UNSUPPORTED_VERSION", 
+                  "DIAMETER_UNABLE_TO_COMPLY", 
+                  "DIAMETER_INVALID_BIT_IN_HEADER", 
+                  "DIAMETER_INVALID_AVP_LENGTH", 
+                  "DIAMETER_INVALID_MESSAGE_LENGTH",
+                  "DIAMETER_INVALID_AVP_BIT_COMBO", 
+                  "DIAMETER_NO_COMMON_SECURITY",]
